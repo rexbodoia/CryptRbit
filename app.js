ce8a8a96c37da4edffd13aa26f60a432cbeb1d65
Whitman Burke <whitmanburke@gmail.com>
2018-08-25 16:52:57 -0700

    Connect with database and set up user route

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
 
 const app = express();
 const db = require('./config/keys').mongoURI;
@@ -9,8 +12,12 @@ mongoose
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+  
+  app.get('/', (req, res) => res.send('Hello World'));
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+  app.use('/api/users', users);
