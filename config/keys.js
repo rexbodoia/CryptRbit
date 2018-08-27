6d6c55e663eabc7d5891f479d672be7c287b0269
Whitman Burke <whitmanburke@gmail.com>
2018-08-27 11:54:22 -0700

    Fix issue with passport

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://aaflex:aaflex1@ds133252.mlab.com:33252/flex-project",
-  secretOrKey: "jsZ2N1Qgl69nd9372eezE40feA0ckhc0"
+  mongoURI: "mongodb://CryptRBit:johnrexwhit1@ds133252.mlab.com:33252/flex-project",
+  secretOrKey: "5qQmsFwc8jzz1GaLpYKAFGB71RKj5qwD"
 };
