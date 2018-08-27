a56661fcd651c7a99f98288df8700a8dcb4fea3e
Whitman Burke <whitmanburke@gmail.com>
2018-08-26 18:55:31 -0700

    Set up validations for form inputs

---
+++ validation/is-empty.js
@@ -0,0 +1,12 @@
+const isEmpty = value => {
+  if (
+    value === undefined ||
+    value === null ||
+    (typeof value === 'object' && Object.keys(value).length === 0) ||
+    (typeof value === 'string' && value.trim().length === 0)
+  ) return true;
+  
+  return false;
+}
+
+module.exports = isEmpty;
