18d6530cfd5eb0fafdce821006ca6cc71651fde4
JohnLockhart <lockhartjohn527@gmail.com>
2018-08-28 12:25:16 -0700

    Add donut reducer to entitites reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import exchangePricesPerCoinPairReducer from './exchange_prices_per_coin_pair_reducer';
 import newsArticlesReducer from "./news_articles_reducer";
+import exchangeDonutReducer from './exchange_donut_reducer';
 
 const entitiesReducer = combineReducers({
   exchangePricesPerCoinPair: exchangePricesPerCoinPairReducer,
-  newsArticles: newsArticlesReducer
+  newsArticles: newsArticlesReducer,
+  exchangeDonutData: exchangeDonutReducer,
 });
 
 export default entitiesReducer;
