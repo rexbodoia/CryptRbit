deb2cbdbd587881c0a812dd78d604e95fc4b9840
Whitman Burke <whitmanburke@gmail.com>
2018-08-27 20:06:58 -0700

    Fix file reference issue in root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import exchangePricesPerCoinPairReducer from './exchange_prices_per_coin_pair_reducer';
-import errorsReducer from './errorsReducer';
+import errorsReducer from './errors_reducer';
 
 const rootReducer = combineReducers({
   errors: errorsReducer,
