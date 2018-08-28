c8280aa1dc244ffb01cec25635279ddf753073d1
Rex Bodoia <rex@bodoia.com>
2018-08-28 09:38:39 -0700

    Add spinner to chart

--- frontend/components/exchange_prices_per_coin_pair.jsx
+++ frontend/components/exchange_prices_per_coin_pair.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ResponsiveContainer, BarChart, XAxis, YAxis, Tooltip, Legend, Bar } from 'recharts';
+import { ClipLoader } from 'react-spinners';
 
 class ExchangePricesPerCoinPair extends React.Component {
   constructor(props) {
@@ -19,19 +20,38 @@ class ExchangePricesPerCoinPair extends React.Component {
     });
   }
 
-  render() {
-    const data = this.props.data;
-    return (
-      <div>
+  renderChart(data) {
+    if (data.length > 0) {
+      return (
         <ResponsiveContainer width="60%" height={250}>
           <BarChart width={730} height={250} data={data}>
             <XAxis dataKey="MARKET" />
-            <YAxis domain={['dataMin', 'dataMax']} />
+            <YAxis domain={['dataMin - 20', 'dataMax']} />
             <Tooltip />
             <Legend />
             <Bar dataKey="PRICE" fill="#8884d8" />
           </BarChart>
         </ResponsiveContainer>
+      );
+    } else {
+      return (
+        <div style={{ height: 160, marginLeft: 400, marginTop: 80 }}>
+          <ClipLoader
+            className='spinner'
+            sizeUnit={"px"}
+            size={70}
+            color={'rgb(155, 166, 178)'}
+          />
+        </div>
+      )
+    }
+  }
+
+  render() {
+    const data = this.props.data;
+    return (
+      <div>
+        {this.renderChart(data)}
       </div>
     )
   }
