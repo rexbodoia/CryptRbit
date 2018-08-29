bc0ed73430aa4392cf517e9800ad3006252fe3be
Rex Bodoia <rex@bodoia.com>
2018-08-28 19:08:45 -0700

    Move form outside of my component and add changing the currenct pair to the redux cycle and global state

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 import ExchangePricesPerCoinPairContainer from './exchange_prices_per_coin_pair/exchange_prices_per_coin_pair_container';
+import CoinPairFormContainer from './coin_pair_form/coin_pair_form_container';
 
 import NewsFeedContainer from './news_feed/news_feed_container';
 
 const App = () => (
   <div>
+    <CoinPairFormContainer />
     <ExchangePricesPerCoinPairContainer />
     <NewsFeedContainer />
   </div>
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import exchangePricesPerCoinPairReducer from './exchange_prices_per_coin_pair_reducer';
 import newsArticlesReducer from "./news_articles_reducer";
+import coinPairReducer from "./coin_pair_reducer";
 
 const entitiesReducer = combineReducers({
   exchangePricesPerCoinPair: exchangePricesPerCoinPairReducer,
-  newsArticles: newsArticlesReducer
+  newsArticles: newsArticlesReducer,
+  coinPair: coinPairReducer
 });
 
 export default entitiesReducer;
