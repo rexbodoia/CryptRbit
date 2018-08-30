207e20e30db532e597b4c2e1121ad6dbf75b8ca8
JohnLockhart <lockhartjohn527@gmail.com>
2018-08-29 19:58:59 -0700

    Add session functions to the entry.jsx file to set session and user

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -5,18 +5,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import configureStore from './store/store';
 import Root from './components/root';
-import {
-  fetchTopExchangeForDonut,
-  fetchTotalVolumeForDonut
-} from "./actions/exchange_donut_actions";
+import * as APIUtil from './api_utils/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch; 
-  window.fetchTop = fetchTopExchangeForDonut;
-  window.fetchTotal = fetchTotalVolumeForDonut;
+  let store = configureStore();
+  //check for token
+  if (localStorage.jwtToken) {
+    //set up the header with the token
+    APIUtil.setAuthToken(localStorage.jwtToken);
+    //Decode token and access the user's info 
+    const decoded = jwt_decode(localStorage.jwtToken);
+    //Set the user (? and 'isAuthenticated')
+    store.dispatch(APIUtil.setCurrentUser(decoded));
 
+    //check for expired token
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+      //Logout the user
+      store.dispatch(APIUtil.logoutUser());
+      //Redirect to login
+      window.location.href = '/login';
+    }
+  }
+  
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+//online example imports and calls 'registerServiceWorker' but no explanation, so left it out
