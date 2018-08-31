10f809041fee1288f6779eb55a8fd529efcbf5a2
JohnLockhart <lockhartjohn527@gmail.com>
2018-08-31 12:21:12 -0700

    update keys to two files, verify app still connecting to mLAB

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose // Connect to MongoDB
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
-// Set port for app to listen on
+// Set port for app to listen on 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
     
