36d79b3db92d26e95d9500afa3791847138b831f
Rex Bodoia <rex@bodoia.com>
2018-09-01 19:17:54 -0700

    Fix keys conflict

--- config/keys.js
+++ config/keys.js
@@ -1,17 +1,11 @@
-module.exports = {
-  mongoURI: "mongodb://CryptRBit:johnrexwhit1@ds133252.mlab.com:33252/flex-project",
-  secretOrKey: "5qQmsFwc8jzz1GaLpYKAFGB71RKj5qwD"
-};
-
 // module.exports = {
 //     mongoURI: "mongodb://CryptRBit:johnrexwhit1@ds133252.mlab.com:33252/flex-project",
 //     secretOrKey: "5qQmsFwc8jzz1GaLpYKAFGB71RKj5qwD"
 // };
 //
 // keys.js
-// if (process.env.NODE_ENV === 'production') {
-//     module.exports = require('./keys_prod');
-// } else {
-//     module.exports = require('./keys_dev');
-// }
-// >>>>>>> 3dc3c0e9b9b2c51913ad87fe34c31fd41e6edb75
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
+}
