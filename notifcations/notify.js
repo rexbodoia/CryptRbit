fabe22c54ed7d21ad5146ed86997957feebbe5c1
JohnLockhart <lockhartjohn527@gmail.com>
2018-09-02 14:47:13 -0700

    Begin skeleton for the notifications

---
+++ notifcations/notify.js
@@ -0,0 +1,41 @@
+const runNotifications = () => {
+  
+    // what call back am I going to run?
+
+    setInterval(cb,1hr) {
+
+    }
+
+    const getData = () => {
+        //some type of http req/res to API
+
+        const processData = () => {
+            //filter and reshape - need coin, %d if greater than minimum set on notifications
+            return processedData;
+        }
+        return processedData;
+    }
+
+   
+
+    const checkIfNotifyAnyUser = (processedData) => {
+        //get collection from db
+        const getNotifications = () => { return list}
+
+        //and compare the processed data by coin and %d
+        const compareListAndData = (processedData, list) => {
+            const sendEmailIfThreshold = () => {
+                //get user email and then fire email notification template off
+            }
+
+
+
+        }
+
+    }
+
+    
+    
+
+
+}
